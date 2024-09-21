2a48a78126e17e5ff1cf958eabe96ff82e0c84c1
Harsh <ph335579@gmail.com>
2024-09-21 13:20:49 +0530

    changes

--- server/controllers/forgotPasswordController.js
+++ server/controllers/forgotPasswordController.js
@@ -21,7 +21,7 @@ const forgotPassword = async (req, res) => {
     await user.save();
     
     //Send email with reset token
-    const resetUrl = `https://todo-app-b96a5.web.app/resetPassword?token=${resetToken}`;
+    const resetUrl = `http://localhost:8001/resetPassword?token=${resetToken}`;
     var transporter = createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
@@ -34,7 +34,7 @@ const forgotPassword = async (req, res) => {
     });
 
     var mailOptions = {
-        from: 'alok.yadav6000@gmail.com',
+        from: 'ph335579@gmail.com',
         to: email,
         subject: "Reset Password",
         html:`<h1>Reset Password</h1><h2>Click on the link to reset your password</h2><h3>${resetUrl}</h3>`
